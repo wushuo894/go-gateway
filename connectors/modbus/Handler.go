fc94cba6816f50e48fbc0498d72c5dc3458dd099
wushuo <2725730722@qq.com>
2024-05-07 14:42:12 +0800

    添加线程锁

--- connectors/modbus/Handler.go
+++ connectors/modbus/Handler.go
@@ -2,17 +2,16 @@ package modbus
 
 import (
 	"encoding/binary"
-	"github.com/goburrow/modbus"
+	"github.com/grid-x/modbus"
+	"log"
 	"strconv"
+	"sync"
+	"time"
 )
 
 func (c ConfigModbus) Func(client modbus.Client, info InfoModbus, value any) (results []byte, err error) {
 	address := info.Address
 	count := info.ObjectsCount
-
-	if err != nil {
-		return nil, err
-	}
 
 	m := map[int]func() (results []byte, err error){
 		modbus.FuncCodeReadDiscreteInputs: func() (results []byte, err error) {
@@ -46,38 +45,58 @@ func (c ConfigModbus) Func(client modbus.Client, info InfoModbus, value any) (re
 	return m[info.FunctionCode]()
 }
 
-var HandleMap = map[string]*modbus.RTUClientHandler{}
+var (
+	HandleMap = map[string]modbus.Client{}
+	LockerMap = map[string]*sync.Mutex{}
+	Locker    = &sync.Mutex{}
+)
 
 func (c ConfigModbus) item(info InfoModbus, value any) (any, error) {
-	var handler *modbus.RTUClientHandler = HandleMap[c.Port]
+	Locker.Lock()
+	client := HandleMap[c.Port+","+strconv.Itoa(c.UnitId)]
+	locker := LockerMap[c.Port]
 
-	if handler == nil {
-		handler = modbus.NewRTUClientHandler(c.Port)
+	if locker == nil {
+		locker = &sync.Mutex{}
+		LockerMap[c.Port] = locker
+	}
+
+	if client == nil {
+		handler := modbus.NewRTUClientHandler(c.Port)
 		handler.BaudRate = c.Baudrate
 		handler.DataBits = c.Databits
 		handler.Parity = c.Parity
 		handler.StopBits = c.Stopbits
-		handler.SlaveId = byte(c.UnitId)
+		handler.SlaveID = byte(c.UnitId)
+		handler.Timeout = 3 * time.Second
 		err := handler.Connect()
 		if err != nil {
+			Locker.Unlock()
+			log.Fatalln("Connect Error:", err)
 			return nil, err
 		}
+		log.Println("Connect ", c.Port)
+		client = modbus.NewClient(handler)
+		HandleMap[c.Port+","+strconv.Itoa(c.UnitId)] = client
 	}
+	Locker.Unlock()
 
-	//defer func(handler *modbus.RTUClientHandler) {
-	//	err := handler.Close()
-	//	if err != nil {
-	//		log.Fatalln(err)
-	//	}
-	//}(handler)
-	client := modbus.NewClient(handler)
+	locker.Lock()
 	results, err := c.Func(client, info, value)
+	locker.Unlock()
 	if err != nil {
 		return nil, err
 	}
+	v := binary.BigEndian.Uint16(results)
 
-	var ok = float32(binary.BigEndian.Uint16(results))
+	var ok = float32(v)
 
-	resultStr := strconv.FormatFloat(float64(ok*info.Take), 'f', 2, 64)
+	take := info.Take
+	if take == 0 {
+		take = 1
+	}
+
+	resultStr := strconv.FormatFloat(float64(ok*take), 'f', 2, 64)
+
 	return resultStr, nil
 }
